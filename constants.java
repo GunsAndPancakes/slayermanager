ac7b65bbd14e6f1725b16654a41e2d5eb2a6b32b
GunsAndPancakes <hackshay96@gmail.com>
2014-07-20 16:26:12 -0400

    Create constants.java

---
+++ constants.java
@@ -0,0 +1,46 @@
+package com.slayer.manager.gui;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Constants {
+
+	public static final String[] MONSTERS = { "Aberrant spectres",
+			"Abyssal demons", "Ankou", "Aquanites", "Ascension members",
+			"Automatons", "Aviansie", "Banshees", "Basilisks", "Bats", "Bears",
+			"Birds", "Black demons", "Black dragons", "Bloodvelds",
+			"Blue dragons", "Brine rats", "Bronze dragons", "Catablepon",
+			"Cave bugs", "Cave crawlers", "Cave horrors", "Cave slimes",
+			"Cockatrices", "Cows", "Crawling Hands", "Crocodiles",
+			"Dagannoths", "Dark beasts", "Desert Lizards",
+			"Desert Strykewyrms", "Dogs", "Dust devils", "Dwarfs",
+			"Earth Warriors", "Elves", "Fever Spiders", "Fire giants",
+			"Flesh Crawlers", "Fungal mages", "Gargoyles",
+			"Ganodermic creatures", "Ghosts", "Ghouls", "Goblins", "Goraks",
+			"Greater demons", "Green dragons", "Grifolapines", "Grifolaroos",
+			"Grotworms", "Harpie Bug Swarms", "Hellhounds", "Hill giants",
+			"Hobgoblins", "Icefiends", "Ice giants", "Ice Strykewyrms",
+			"Ice warriors", "Infernal Mages", "Iron dragons", "Jellies",
+			"Jungle horrors", "Jungle Strykewyrms", "Kalphites", "Killerwatts",
+			"Kurasks", "Lesser demons", "Living rock creatures",
+			"Mithril dragons", "Minotaurs", "Mogres", "Molanisks", "Monkeys",
+			"Moss giants", "Mutated jadinko", "Nechryaels", "Ogres",
+			"Otherworldly Beings", "Pyrefiends", "Rats", "Red dragons",
+			"Rockslugs", "Scabarites", "Scorpions", "Sea snakes", "Shades",
+			"Shadow warriors", "Skeletal Wyverns", "Skeletons", "Spiders",
+			"Spiritual mages", "Spiritual rangers", "Spiritual warriors",
+			"Steel dragons", "Suqahs", "Terror Dogs", "Trolls", "Turoths",
+			"TzHaar", "Vampyres", "Vyrewatch", "Wall beasts",
+			"Warped terrorbirds", "Warped tortoises", "Waterfiends",
+			"Werewolves", "Wolves", "Zombies" };
+
+	public static final String[] MASTERS = { "Turael", "Mazchna", "Chaeldar",
+			"Sumona", "Vannaka", "Duradel", "Kuradel" };
+
+	public static int DEFAULT_LEVEL = 0;
+
+	public static int DEFAULT_MASTER = 0;
+
+}
